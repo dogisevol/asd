f17147e5d2027ea5fd76613ce51b90e70f5032b4
dog.is.evol@gmail.com <dog.is.evol@gmail.com>
2017-05-11 13:57:28 +1000

    Counting sort

--- src/stanford/course3/week2/Clustering.groovy
+++ src/stanford/course3/week2/Clustering.groovy
@@ -1,5 +1,7 @@
 package stanford.course3.week2
 
+import javax.jws.soap.SOAPBinding
+
 class KruskalAlgorithm {
 
 
@@ -39,6 +41,72 @@ class KruskalAlgorithm {
         }
 
         println(result)
+    }
+
+
+    void test(int[] data) {
+
+    }
+
+
+}
+
+int hammingDistance(int x, int y) {
+    int dist = 0;
+    def val = x ^ y;
+
+    while (val != 0) {
+        dist++;
+        val &= val - 1;
+    }
+
+    return dist;
+}
+
+class UFHashMap {
+
+    def parent;
+    def rank;
+    int count;
+
+    UFHashMap(int n) {
+        count = n
+        parent = [:]
+        rank = [:]
+    }
+
+
+    int find(int p) {
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        }
+        return p;
+    }
+
+    boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    void init(int item) {
+        rank[item] = 0
+        parent[item] = item
+    }
+
+    void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ) {
+            return
+        }
+
+        if (rank[rootP] < rank[rootQ]) parent[rootP] = rootQ;
+        else if (rank[rootP] > rank[rootQ]) parent[rootQ] = rootP;
+        else {
+            parent[rootQ] = rootP;
+            rank[rootP]++;
+        }
+        count--;
     }
 }
 
@@ -121,6 +189,7 @@ class ClusteringEdge implements Comparable {
     }
 }
 
+
 List<ClusteringEdge> edges = []
 Scanner scanner = new Scanner(new File("clustering1.txt"));
 def nodesNumber = scanner.nextInt()
@@ -134,3 +203,63 @@ while (scanner.hasNext()) {
 }
 
 new KruskalAlgorithm().find(4, edges, nodesNumber)
+
+
+int[] getBitPermutations(int size, int number) {
+    def val = 1
+    int[] result = new int[size]
+    for (int i = 0; i < size; i++) {
+        result[i] = number ^ val
+        val = val << 1
+    }
+    result
+}
+
+scanner = new Scanner(new File("clustering_big.txt"));
+def n = scanner.nextInt()
+size = scanner.nextInt()
+def uf = new UFHashMap(n)
+boolean[] array = new boolean[2.power(size)]
+def c = 0
+while (scanner.hasNext()) {
+    if (c++ % 100 == 0) print "."
+    if (c % 10000 == 0) println c
+    def s = "";
+    (1..size).each {
+        s += scanner.nextInt()
+    }
+    def item = Integer.parseUnsignedInt(s, 2)
+    if (array[item] == true) {
+        uf.count--
+        continue
+    }
+    uf.init(item)
+    array[item] = true
+
+    getBitPermutations(size, item).each {
+        variant1 ->
+            getBitPermutations(size, variant1).each {
+                variant2 ->
+                    if (array[variant2] && !uf.connected(item, variant2)) {
+                        uf.union(item, variant2)
+                    }
+            }
+            if (array[variant1] && !uf.connected(item, variant1)) {
+                uf.union(item, variant1)
+            }
+    }
+}
+
+//array.eachWithIndex { e, i ->
+//    if (i % 10 == 0) print "."
+//    if (i % 1000 == 0) println i
+//    map.get(e).each { o ->
+//        if (array.contains(o)) {
+//            uf.union(e, o)
+//        }
+//    }
+//}
+
+println uf.count
+
+
