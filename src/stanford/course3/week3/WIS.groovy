a3236000ff4efe92517af24c78ee57fb5ca4af9e
dog.is.evol@gmail.com <dog.is.evol@gmail.com>
2017-05-12 15:13:33 +1000

    week3 assignments

--- src/stanford/course3/week3/WIS.groovy
+++ src/stanford/course3/week3/WIS.groovy
@@ -12,34 +12,34 @@ while (scanner.hasNext()) {
 i = array.length - 1
 byte result = 0
 def set = []
-while (i >= 1) {
-    if (array[i] == array[i - 1])
+while (i > 1) {
+    if (array[i-1] >= array[i - 2])
         i--
     else {
         switch (i+1) {
             case 1:
-                result = result | (1 << 1);
+                result = result | (1 << 7);
                 break
             case 2:
-                result = result | (1 << 2);
+                result = result | (1 << 6);
                 break
             case 3:
-                result = result | (1 << 3);
+                result = result | (1 << 5);
                 break
             case 4:
                 result = result | (1 << 4);
                 break
             case 17:
-                result = result | (1 << 5);
+                result = result | (1 << 3);
                 break
             case 117:
-                result = result | (1 << 6);
+                result = result | (1 << 2);
                 break
             case 517:
-                result = result | (1 << 7);
+                result = result | (1 << 1);
                 break
             case 997:
-                result = result | (1 << 8);
+                result = result | (1 << 0);
                 break
         }
         set.add(array[i])
